64af82e68cb38daaec126a9d29b0bc491b0f1333
peter0618 <peterchoi0618@gmail.com>
2020-12-23 08:15:47 +0900

    user 단건 조회 API 작성

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Logger } from '@nestjs/common';
+import { Controller, Get, Logger, Param } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './model/user.model';
 
@@ -10,10 +10,20 @@ export class UserController {
 
   /**
    * 사용자 목록을 조회합니다.
+   * TODO : 검색조건을 적용합니다. (https://github.com/typeorm/typeorm/issues/3103 참고)
    */
   @Get()
   async getList(): Promise<[User]> {
     this.logger.debug(`getList()`);
     return await this.userService.getList();
+  }
+
+  /**
+   * 사용자를 조회합니다.
+   */
+  @Get(':id')
+  async getById(@Param('id') id: number): Promise<User>{
+    this.logger.debug(`getById(id: ${id})`);
+    return await this.userService.getById(id);
   }
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -49,4 +49,46 @@ select id,
 
     return null;
   }
+
+  /**
+   * 사용자를 조회합니다.
+   */
+  async getById(id: number): Promise<User> {
+    try {
+      // fixme : 날짜를 RFC3339 format 으로 변환해서 response 하는 방법을 적용해야 합니다.
+      const rawRows = await this.connection.query(
+        `
+select id,
+       name,
+       phone,
+       email,
+       is_used,
+       is_deleted,
+       date_format(updated_at, '%Y-%m-%d') as updated_at,
+       date_format(created_at, '%Y-%m-%d') as created_at
+  from user
+ where id in (?)`,
+        [id],
+      );
+
+      const row = JsonUtil.snakeToCamelCase(rawRows[0]);
+
+      const { name, phone, email, isUsed, isDeleted, updatedAt, createdAt } = row;
+
+      return {
+        id,
+        name,
+        phone,
+        email,
+        isUsed: isUsed == 1,
+        isDeleted: isDeleted == 1,
+        updatedAt,
+        createdAt,
+      };
+    } catch (e) {
+      this.logger.error(e.toString());
+    }
+
+    return null;
+  }
 }
