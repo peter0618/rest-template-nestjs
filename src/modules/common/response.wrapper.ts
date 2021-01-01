97ca67cef191d3c49cea1a62044a88fcb513e320
peter0618 <peterchoi0618@gmail.com>
2021-01-02 03:04:40 +0900

    REST API 기본 응답포맷 작성

---
+++ src/modules/common/response.wrapper.ts
@@ -0,0 +1,34 @@
+/**
+ * REST API 에서 응답할 때 사용할 기본 포맷을 작성했습니다.
+ * 기본적으로 API 요청 처리 성공 여부(success), 조회 데이터(data), 에러메시지(message), 에러메시지코드(code)를 리턴합니다.
+ * TODO : 에러메시지 코드에 대한 상세 정의가 필요합니다.
+ */
+export class ResponseWrapper<T> {
+  private success = true;
+
+  private data?: T;
+
+  private message?: string;
+
+  private code?: string;
+
+  public fail(): ResponseWrapper<T> {
+    this.success = false;
+    return this;
+  }
+
+  public setData(data: T): ResponseWrapper<T> {
+    this.data = data;
+    return this;
+  }
+
+  public setMessage(message: string): ResponseWrapper<T> {
+    this.message = message;
+    return this;
+  }
+
+  public setCode(code: string): ResponseWrapper<T> {
+    this.code = code;
+    return this;
+  }
+}
