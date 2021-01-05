cf7557bb2fd127ad9ef86f203e52c1c724ab3b75
peter0618 <peterchoi0618@gmail.com>
2021-01-06 01:05:29 +0900

    swagger 패키지 설치 및 기본 설정 적용

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { UserModule } from './modules/user/user.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -10,6 +12,18 @@ async function bootstrap() {
   const logger = app.get<Logger, Logger>(Logger);
 
   const port = process.env.APP_PORT || 3000;
+
+  const options = new DocumentBuilder()
+    .setTitle('Users')
+    .setDescription('The users API description')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+  const document = SwaggerModule.createDocument(app, options, {
+    include: [UserModule]
+  });
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port, () => {
     logger.setContext('APP');
     logger.log(`Server is listening on : ${port}`);
